e6714a34740a11a6e77a796bf3fa871097d96344
smalex-als <smalex69@gmail.com>
2021-07-06 10:04:58 -0400

    update

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,13 +1,31 @@
 module.exports = {
   root: true,
+  env: {
     es6: true,
     node: true,
   },
-  "extends": [
+  extends: [
     "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "google",
+    "plugin:@typescript-eslint/recommended",
   ],
-  "rules": {
-    quotes: ["error", "double"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "require-jsdoc": "off",
   },
 };
